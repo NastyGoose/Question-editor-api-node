7be50b6aac7719db66d8108c4aa44952d370964d
Artem <artyom.balamutyuk@mail.ru>
2018-12-24 23:13:55 +0300

    Add cors and config starter

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const config = require("config");
 const express = require("express");
 const app = express();
 
+require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
+require("./startup/config")();
 require("./startup/validation")();
 
 const port = process.env.PORT || config.get("port");
