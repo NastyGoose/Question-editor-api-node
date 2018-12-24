0e96fbe152dde87c206397e0f57a6df5e25b5b91
Artem <artyom.balamutyuk@mail.ru>
2018-12-24 12:30:00 +0300

    Add editor route

--- routes/editor.js
+++ routes/editor.js
@@ -1,92 +1,76 @@
 const express = require("express");
 const router = express.Router();
 const {
-    Test,
-    validator
+	Test,
+	validator
 } = require("../models/test");
+const {
+	User
+} = require("../models/user");
 const validate = require("../middleware/validate");
 const validateObjectId = require("../middleware/validateObjectId");
 
 const debug = require("debug")("node:editor");
-const getCurrentTime = require("../utils/getCurrentTime");
-const removeUndefined = require("../utils/removeUndefined");
+const getCurrentTime = require("../utils/time/getCurrentTime");
 
 router.get("/:id", [validateObjectId], async (req, res) => {
-    debug("Trying to get with id");
-    const {
-        id
-    } = req.params;
-    const test = await Test.findById(id);
-    if (!test) return res.status(404).send("The test with the given ID was not found.");
+	debug("Trying to get with id");
+	const {
+		id
+	} = req.params;
+	const test = await Test.findById(id);
+	if (!test) return res.status(404).send("The test with the given ID was not found.");
 
-    res.send(test);
-    debug(`Send test with id: ${id} - ${getCurrentTime()}`);
+	res.send(test);
+	debug(`Send test with id: ${id} - ${getCurrentTime()}`);
 });
 
 router.post("/", [validate(validator)], async (req, res) => {
-    debug("Trying to post");
-    const {
-        question,
-        answers,
-        likes,
-        dislikes,
-        visits,
-        description
-    } = req.body;
-    const test = new Test({
-        question,
-        answers,
-        likes,
-        dislikes,
-        visits,
-        description
-    }); // or simply use req.body
-    await test.save();
+	debug("Trying to post");
+	const {
+		question,
+		answers,
+		description
+	} = req.body;
+	const test = new Test({
+		question,
+		answers,
+		author: {
+			author: "5c18a4bc086d3702a4d04db3",
+			name: "Artem"
+		},
+		description
+	});
+	await test.save();
 
-    res.send(test);
-    debug(`Created test: ${question} - ${getCurrentTime()}`);
-});
+	const user = await User.findById("5c18a4bc086d3702a4d04db3");
 
-router.patch("/:id", [validateObjectId, validate(validator, {
-    update: true
-})], async (req, res) => {
-    debug("Trying to patch with id");
-    const {
-        likes,
-        dislikes,
-        visits
-    } = req.body;
+	user.tests.push({
+		test: test._id,
+		isMine: true
+	});
 
-    const updatedData = removeUndefined({
-        likes: likes,
-        dislikes: dislikes,
-        visits: visits
-    });
+	user.save();
 
-    const test = await Test.findOneAndUpdate({
-        _id: req.params.id
-    }, {
-        $set: updatedData
-    }, {
-        new: true
-    });
-    if (!test) return res.status(404).send("The test was not found.")
-
-    res.send(test);
-    debug("Test was updated.");
+	res.send(test);
+	debug(`Created test: ${question} - ${getCurrentTime()}`);
 });
 
 router.put("/:id", [validateObjectId, validate(validator)], async (req, res) => {
-    debug("Trying to put with id");
-    const test = await Test.findOneAndUpdate({
-        _id: req.params.id
-    }, req.body, {
-        new: true
-    });
-    if (!test) return res.status(404).send("The test with the given ID was not found.");
+	debug("Trying to put with id");
+	const test = await Test.findOneAndUpdate({
+		_id: req.params.id,
+		verified: false,
+		patch: {
+			$eq: null
+		}
+	}, req.body, {
+		new: true
+	});
+	if (!test) return res.status(404).send("The test can't be modified after being verified or patched.");
 
-    res.send(test);
-    debug("Test was updated.");
+	res.send(test);
+	debug("Test was updated.");
 });
 
 module.exports = router;
