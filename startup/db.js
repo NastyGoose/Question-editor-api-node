765e1506c79097ae6e110ea4d0148482c6d11f5a
Artem <artyom.balamutyuk@mail.ru>
2019-05-09 20:40:25 +0300

    1

--- startup/db.js
+++ startup/db.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 const debug = require("debug")("node:db");
 const config = require("config");
 
-module.exports = function () {
-    const uri = config.get("db.uri");
-    const options = config.get("db.options");
-    mongoose.connect(uri, options)
-        .then(() => debug(`Connected to ${uri}...`));
-}
+module.exports = function() {
+	const uri = process.env.DB_URI || config.get("db.uri");
+	const options = config.get("db.options");
+	mongoose.connect(uri, options).then(() => debug(`Connected to ${uri}...`));
+};
